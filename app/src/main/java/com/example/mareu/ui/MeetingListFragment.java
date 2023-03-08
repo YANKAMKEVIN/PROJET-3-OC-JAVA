f5650e741ba2ef9a7e8a220e30f58148b3820b81
Kevin Yankam <yankamkevin95@gmail.com>
2023-03-08 21:44:47 +0100

    commit MeetingListFragment used to display the list of meetings

---
+++ app/src/main/java/com/example/mareu/ui/MeetingListFragment.java
@@ -0,0 +1,38 @@
+package com.example.mareu.ui;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mareu.R;
+
+public class MeetingListFragment extends Fragment {
+
+    private MeetingListViewModel mViewModel;
+
+    public static MeetingListFragment newInstance() {
+        return new MeetingListFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_meeting_list, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MeetingListViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
