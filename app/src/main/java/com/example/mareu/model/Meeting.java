a0431b712b78e85dfd4a786846c8b842ddabf7fd
Kevin Yankam <yankamkevin95@gmail.com>
2023-03-08 16:45:09 +0100

    commit Meeting Model

---
+++ app/src/main/java/com/example/mareu/model/Meeting.java
@@ -0,0 +1,86 @@
+package com.example.mareu.model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+public class Meeting {
+
+    private String name;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private LocalDate date;
+    private String location;
+    private String subject;
+    private ArrayList<String> participants;
+
+
+    public Meeting(String name, LocalTime startTime, LocalTime endTime, LocalDate date, String location, String subject, ArrayList<String> participants) {
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.date = date;
+        this.location = location;
+        this.subject = subject;
+        this.participants = participants;
+    }
+
+    // --- GETTERS AND SETTERS ---
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public ArrayList<String> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(ArrayList<String> participants) {
+        this.participants = participants;
+    }
+
+}
