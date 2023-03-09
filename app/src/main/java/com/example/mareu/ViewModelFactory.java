970ffa9bb9b134607681379f9c37efc128331f94
Kevin Yankam <yankamkevin95@gmail.com>
2023-03-09 13:20:09 +0100

    commit ViewModelFactory Singleton Implementation

--- app/src/main/java/com/example/mareu/ViewModelFactory.java
+++ app/src/main/java/com/example/mareu/ViewModelFactory.java
@@ -4,34 +4,28 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.mareu.api.ApiService;
+import com.example.mareu.api.FakeApiService;
 import com.example.mareu.repository.MeetingRepository;
 import com.example.mareu.ui.MeetingListViewModel;
 
 public class ViewModelFactory implements ViewModelProvider.Factory {
 
+    private final MeetingRepository meetingRepository;
     private static volatile ViewModelFactory factory;
+
+    public ViewModelFactory(@NonNull MeetingRepository meetingRepository) {
+        this.meetingRepository = meetingRepository;
+    }
 
     public static ViewModelFactory getInstance() {
         if (factory == null) {
             synchronized (ViewModelFactory.class) {
                 if (factory == null) {
-
-                    /*factory = new ViewModelFactory(
-                        //    new MeetingRepository(new ApiService())
-                    );*/
+                    factory = new ViewModelFactory(new MeetingRepository(new FakeApiService()));
                 }
             }
         }
-
         return factory;
-    }
-
-    @NonNull
-    private final MeetingRepository meetingRepository;
-
-    public ViewModelFactory(@NonNull MeetingRepository meetingRepository) {
-        this.meetingRepository = meetingRepository;
     }
 
     @SuppressWarnings("unchecked")
