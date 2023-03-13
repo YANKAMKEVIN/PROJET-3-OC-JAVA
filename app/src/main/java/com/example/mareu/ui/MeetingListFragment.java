7092dcc0eac44a49ab1b7b7e305ef56937dbc6a1
Kevin Yankam <yankamkevin95@gmail.com>
2023-03-13 11:35:40 +0100

    commit MeetingListFragment Implementing the use of the viewModel inside the Controler.

--- app/src/main/java/com/example/mareu/ui/MeetingListFragment.java
+++ app/src/main/java/com/example/mareu/ui/MeetingListFragment.java
@@ -1,23 +1,25 @@
 package com.example.mareu.ui;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.mareu.R;
+import com.example.mareu.ViewModelFactory;
 
 public class MeetingListFragment extends Fragment {
 
     private MeetingListViewModel mViewModel;
-
+    private RecyclerView mRecyclerView;
     public static MeetingListFragment newInstance() {
         return new MeetingListFragment();
     }
@@ -25,14 +27,19 @@ public class MeetingListFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_meeting_list, container, false);
-    }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(MeetingListViewModel.class);
-        // TODO: Use the ViewModel
+        View view = inflater.inflate(R.layout.fragment_meeting_list, container, false);
+        mViewModel = new ViewModelProvider(this, ViewModelFactory.getInstance()).get(MeetingListViewModel.class);
+        mRecyclerView = view.findViewById(R.id.recycler_cv);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+
+        mViewModel.getMeetings().observe(getViewLifecycleOwner(), meetings1 -> {
+            MeetingAdapter monAdapter = new MeetingAdapter(meetings1);
+            mRecyclerView.setAdapter(monAdapter);
+        });
+
+        return view;
     }
 
 }
