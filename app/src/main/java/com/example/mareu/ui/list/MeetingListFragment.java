ad64ab3113819d4e375b6979bb0ddb99491010ed
Kevin Yankam <yankamkevin95@gmail.com>
2023-03-14 18:28:08 +0100

    [MeetingFragment] Refresh the list after a delete

--- app/src/main/java/com/example/mareu/ui/list/MeetingListFragment.java
+++ app/src/main/java/com/example/mareu/ui/list/MeetingListFragment.java
@@ -1,6 +1,7 @@
 package com.example.mareu.ui.list;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,11 +16,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mareu.R;
 import com.example.mareu.ViewModelFactory;
+import com.example.mareu.model.Meeting;
+
+import java.util.Objects;
 
 public class MeetingListFragment extends Fragment {
 
     private MeetingListViewModel mViewModel;
     private RecyclerView mRecyclerView;
+    private onMeetingCLickListener listener;
     public static MeetingListFragment newInstance() {
         return new MeetingListFragment();
     }
@@ -32,14 +37,28 @@ public class MeetingListFragment extends Fragment {
         mViewModel = new ViewModelProvider(this, ViewModelFactory.getInstance()).get(MeetingListViewModel.class);
         mRecyclerView = view.findViewById(R.id.recycler_cv);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
-        mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL));
 
+        listener = new onMeetingCLickListener() {
+            @Override
+            public void onDeleteMeetingClicked(Meeting meeting) {
+                mViewModel.deleteMeeting(meeting);
+                MeetingListAdapter monAdapter = new MeetingListAdapter( listener);
+                mViewModel.getMeetings().observe(getViewLifecycleOwner(), meetings1 -> {
+                    Log.d("KEVIN", String.valueOf(meetings1.size()));
+                    monAdapter.submitList(meetings1);
+                });
+                mRecyclerView.setAdapter(monAdapter);
+            }
+        };
+
+        MeetingListAdapter monAdapter = new MeetingListAdapter( listener);
         mViewModel.getMeetings().observe(getViewLifecycleOwner(), meetings1 -> {
-            MeetingAdapter monAdapter = new MeetingAdapter(meetings1);
-            mRecyclerView.setAdapter(monAdapter);
+            Log.d("KEVIN", String.valueOf(meetings1.size()));
+            monAdapter.submitList(meetings1);
         });
+        mRecyclerView.setAdapter(monAdapter);
 
         return view;
     }
-
 }
