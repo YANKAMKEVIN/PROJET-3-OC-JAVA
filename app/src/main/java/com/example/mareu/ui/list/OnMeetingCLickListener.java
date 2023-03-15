7baa72f1216c194f818f40b7306fbddb99576613
Kevin Yankam <yankamkevin95@gmail.com>
2023-03-15 15:24:53 +0100

    [MeetingFragment] Rename the interface OnMeetingCLickListener

---
+++ app/src/main/java/com/example/mareu/ui/list/OnMeetingCLickListener.java
@@ -0,0 +1,5 @@
+package com.example.mareu.ui.list;
+
+public interface OnMeetingCLickListener {
+    void onDeleteMeetingClicked(long meetingId);
+}
--- app/src/main/java/com/example/mareu/ui/list/onMeetingCLickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.mareu.ui.list;
-
-import com.example.mareu.model.Meeting;
-
-public interface onMeetingCLickListener {
-    void onDeleteMeetingClicked(Meeting meeting);
-}
