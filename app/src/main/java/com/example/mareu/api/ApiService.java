a1a21f2a18fc0301b44398039726ffacde36ee55
Kevin Yankam <yankamkevin95@gmail.com>
2023-03-17 15:02:17 +0100

    [ApiService] Add new methods in the Api

--- app/src/main/java/com/example/mareu/api/ApiService.java
+++ app/src/main/java/com/example/mareu/api/ApiService.java
@@ -1,10 +1,25 @@
 package com.example.mareu.api;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.LiveData;
+
 import com.example.mareu.model.Meeting;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 public interface ApiService {
-    List<Meeting> getMeetings();
-    void deleteMeeting(Meeting meeting);
+    LiveData<List<Meeting>> getMeetingsLiveData();
+    void deleteMeeting(long meetingId);
+    void addMeeting(
+            @NonNull String name,
+            @NonNull LocalTime startTime,
+            @Nullable LocalDate date,
+            @Nullable String location,
+            @Nullable String subject,
+            @Nullable List<String> participants
+    );
+    void generateFakeMeetings();
 }
