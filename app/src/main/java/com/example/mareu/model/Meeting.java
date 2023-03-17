2c6b81999fb9255c5fe030ee27cc5c2fb5615cb8
Kevin Yankam <yankamkevin95@gmail.com>
2023-03-17 12:39:58 +0100

    [Meeting] Remove the endTime for a Meeting

--- app/src/main/java/com/example/mareu/model/Meeting.java
+++ app/src/main/java/com/example/mareu/model/Meeting.java
@@ -11,18 +11,16 @@ public class Meeting {
     private long id;
     private String name;
     private LocalTime startTime;
-    private LocalTime endTime;
     private LocalDate date;
     private String location;
     private String subject;
     private List<String> participants;
 
 
-    public Meeting(long id, String name, LocalTime startTime, LocalTime endTime, LocalDate date, String location, String subject, List<String> participants) {
+    public Meeting(long id, String name, LocalTime startTime, LocalDate date, String location, String subject, List<String> participants) {
         this.id = id;
         this.name = name;
         this.startTime = startTime;
-        this.endTime = endTime;
         this.date = date;
         this.location = location;
         this.subject = subject;
@@ -53,14 +51,6 @@ public class Meeting {
 
     public void setStartTime(LocalTime startTime) {
         this.startTime = startTime;
-    }
-
-    public LocalTime getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(LocalTime endTime) {
-        this.endTime = endTime;
     }
 
     public LocalDate getDate() {
@@ -100,11 +90,11 @@ public class Meeting {
         if (this == o) return true;
         if (!(o instanceof Meeting)) return false;
         Meeting meeting = (Meeting) o;
-        return getId() == meeting.getId() && getName().equals(meeting.getName()) && getStartTime().equals(meeting.getStartTime()) && getEndTime().equals(meeting.getEndTime()) && getDate().equals(meeting.getDate()) && getLocation().equals(meeting.getLocation()) && getSubject().equals(meeting.getSubject()) && getParticipants().equals(meeting.getParticipants());
+        return getId() == meeting.getId() && getName().equals(meeting.getName()) && getStartTime().equals(meeting.getStartTime()) && getDate().equals(meeting.getDate()) && getLocation().equals(meeting.getLocation()) && getSubject().equals(meeting.getSubject()) && getParticipants().equals(meeting.getParticipants());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, startTime, endTime, date, location, subject, participants);
+        return Objects.hash(id, name, startTime, date, location, subject, participants);
     }
 }
