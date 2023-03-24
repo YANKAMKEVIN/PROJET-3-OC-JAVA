64fc71d5cbc830894812854e451b0b7826ea799e
Kevin Yankam <yankamkevin95@gmail.com>
2023-03-24 14:45:39 +0100

    [MainActivity] Implement filters

--- app/src/main/java/com/example/mareu/MainActivity.java
+++ app/src/main/java/com/example/mareu/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mareu;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,18 +11,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mareu.databinding.ActivityMainBinding;
 import com.example.mareu.ui.add.AddMeetingActivity;
+import com.example.mareu.ui.list.DateFilterEvent;
 import com.example.mareu.ui.list.LocationFilterEvent;
+import com.example.mareu.ui.list.ResetFilterEvent;
 
 import org.greenrobot.eventbus.EventBus;
 
-public class MainActivity extends AppCompatActivity {
+import java.time.LocalDate;
 
-    private ActivityMainBinding binding;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
         binding.mainFabAdd.setOnClickListener(v -> startActivity(AddMeetingActivity.navigate(this)));
@@ -39,23 +42,45 @@ public class MainActivity extends AppCompatActivity {
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.action_filter_by_location:
-                showFilterDialog();
+                showLocationFilterDialog();
+                return true;
+            case R.id.action_filter_by_date:
+                showDateFilterDialog();
+                return true;
+            case R.id.action_reset_filter:
+                resetFilterDialog();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    private void showFilterDialog() {
+    private void resetFilterDialog() {
+        EventBus.getDefault().post(new ResetFilterEvent());
+    }
+
+    private void showLocationFilterDialog() {
         // Show a dialog or dropdown list to allow the user to select a location to filter the list of meetings
         // Then update the list of meetings based on the selected location
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
         builder.setTitle(R.string.app_name);
-        builder.setItems(R.array.salles, (dialog, which) -> {
-            String[] locations = getResources().getStringArray(R.array.salles);
+        builder.setItems(R.array.rooms, (dialog, which) -> {
+            String[] locations = getResources().getStringArray(R.array.rooms);
             String selectedLocation = locations[which];
             EventBus.getDefault().post(new LocationFilterEvent(selectedLocation));
         });
         builder.show();
+    }
+
+    private void showDateFilterDialog() {
+        // Show a dialog to allow the user to select a date to filter the list of meetings
+        // Then update the list of meetings based on the selected date
+        LocalDate today = LocalDate.now();
+        DatePickerDialog dialog = new DatePickerDialog(this, (view, year, monthOfYear, dayOfMonth) -> {
+            LocalDate selectedDate = LocalDate.of(year, monthOfYear + 1, dayOfMonth);
+            EventBus.getDefault().post(new DateFilterEvent(selectedDate));
+        }, today.getYear(), today.getMonthValue() - 1, today.getDayOfMonth());
+        dialog.show();
     }
 }
